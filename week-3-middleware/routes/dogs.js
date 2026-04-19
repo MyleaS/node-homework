b6b2b9d0fa5a8183ad62da91e9850a915abd926f
Mylea Spicer <mylea2012@gmail.com>
2026-04-18 20:48:52 -0400

    Complete assignment 3 - middleware and error handling

--- week-3-middleware/routes/dogs.js
+++ week-3-middleware/routes/dogs.js
@@ -1,30 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const dogs = require("../dogData.js");
-
-class ValidationError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'ValidationError';
-    this.statusCode = 400;
-  }
-}
-
-class NotFoundError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'NotFoundError';
-    this.statusCode = 404;
-  }
-}
-
-class UnauthorizedError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'UnauthorizedError';
-    this.statusCode = 401;
-  }
-}
+const { ValidationError, NotFoundError } = require("../errors");
 
 router.get("/dogs", (req, res) => {
   res.json(dogs);
