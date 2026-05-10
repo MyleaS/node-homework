4ed85a1c6e74f0a864d4a549420194c37bc87b71
Mylea Spicer <mylea2012@gmail.com>
2026-05-10 01:16:42 -0400

    Fix: add PrismaClientInitializationError handler to error middleware

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,13 +1,27 @@
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err.code === "ECONNREFUSED" && err.port === 5432) {
-    console.log("The database connection was refused.  Is your database service running?");
+  // ADDED: Prisma connection error check
+  if (err.name === "PrismaClientInitializationError") {
+    console.error("Couldn't connect to the database. Is it running?");
+    if (!res.headersSent) {
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ message: "Database connection failed." });
+    }
   }
+
+  // Existing pg connection error check
+  if (err.code === "ECONNREFUSED" && err.port === 5432) {
+    console.log(
+      "The database connection was refused. Is your database service running?"
+    );
+  }
+
   console.error(
     "Internal server error: ",
     err.constructor.name,
-    JSON.stringify(err, ["name", "message", "stack"]),
+    JSON.stringify(err, ["name", "message", "stack"])
   );
 
   if (!res.headersSent) {
