6fdd2fad4bb930c5f04570bc3215c302e69aafa0
Mylea Spicer <mylea2012@gmail.com>
2026-05-29 22:53:04 -0400

    Assignment 9: Automated Testing

---
+++ test/validation.test.js
@@ -0,0 +1,122 @@
+const { userSchema } = require("../validation/userSchema");
+const { taskSchema, patchTaskSchema } = require("../validation/taskSchema");
+
+describe("user object validation tests", () => {
+  it("1. doesn't permit a trivial password", () => {
+    const { error } = userSchema.validate(
+      { name: "Bob", email: "bob@sample.com", password: "password" },
+      { abortEarly: false }
+    );
+    expect(
+      error.details.find((detail) => detail.context.key == "password")
+    ).toBeDefined();
+  });
+
+  it("2. requires that an email be specified", () => {
+    const { error } = userSchema.validate(
+      { name: "Bob", password: "Pa$$word20" },
+      { abortEarly: false }
+    );
+    expect(
+      error.details.find((detail) => detail.context.key == "email")
+    ).toBeDefined();
+  });
+
+  it("3. does not accept an invalid email", () => {
+    const { error } = userSchema.validate(
+      { name: "Bob", email: "notanemail", password: "Pa$$word20" },
+      { abortEarly: false }
+    );
+    expect(
+      error.details.find((detail) => detail.context.key == "email")
+    ).toBeDefined();
+  });
+
+  it("4. requires a password", () => {
+    const { error } = userSchema.validate(
+      { name: "Bob", email: "bob@sample.com" },
+      { abortEarly: false }
+    );
+    expect(
+      error.details.find((detail) => detail.context.key == "password")
+    ).toBeDefined();
+  });
+
+  it("5. requires name", () => {
+    const { error } = userSchema.validate(
+      { email: "bob@sample.com", password: "Pa$$word20" },
+      { abortEarly: false }
+    );
+    expect(
+      error.details.find((detail) => detail.context.key == "name")
+    ).toBeDefined();
+  });
+
+  it("6. name must be valid (3 to 30 characters)", () => {
+    const { error } = userSchema.validate(
+      { name: "Bo", email: "bob@sample.com", password: "Pa$$word20" },
+      { abortEarly: false }
+    );
+    expect(
+      error.details.find((detail) => detail.context.key == "name")
+    ).toBeDefined();
+  });
+
+  it("7. valid user object returns falsy error", () => {
+    const { error } = userSchema.validate(
+      { name: "Bob", email: "bob@sample.com", password: "Pa$$word20" },
+      { abortEarly: false }
+    );
+    expect(error).toBeFalsy();
+  });
+});
+
+describe("task object validation tests", () => {
+  it("8. task schema requires a title", () => {
+    const { error } = taskSchema.validate({}, { abortEarly: false });
+    expect(
+      error.details.find((detail) => detail.context.key == "title")
+    ).toBeDefined();
+  });
+
+  it("9. isCompleted must be valid if specified", () => {
+    const { error } = taskSchema.validate(
+      { title: "My Task", isCompleted: "notabool" },
+      { abortEarly: false }
+    );
+    expect(
+      error.details.find((detail) => detail.context.key == "isCompleted")
+    ).toBeDefined();
+  });
+
+  it("10. isCompleted defaults to false if not specified", () => {
+    const { value } = taskSchema.validate(
+      { title: "My Task" },
+      { abortEarly: false }
+    );
+    expect(value.isCompleted).toBe(false);
+  });
+
+  it("11. isCompleted remains true if set to true", () => {
+    const { value } = taskSchema.validate(
+      { title: "My Task", isCompleted: true },
+      { abortEarly: false }
+    );
+    expect(value.isCompleted).toBe(true);
+  });
+});
+
+describe("patchTaskSchema validation tests", () => {
+  it("12. patchTaskSchema does not require a title", () => {
+    const { error } = patchTaskSchema.validate(
+      { isCompleted: true },
+      { abortEarly: false }
+    );
+    expect(error).toBeFalsy();
+  });
+
+  it("13. isCompleted remains undefined if not provided", () => {
+    const { value } = patchTaskSchema.validate({}, { abortEarly: false });
+    expect(value.isCompleted).toBeUndefined();
+  });
+});
